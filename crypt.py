7bc7961d217c2f81c5f3160aa5a6bc5fa756e014
Andrew Mather <12123941+Andreww72@users.noreply.github.com>
2021-09-06 18:28:46 +1000

    Challenge 3

--- crypt.py
+++ crypt.py
@@ -17,3 +17,24 @@ def fixed_xor(a: bytes, b: bytes) -> bytes:
 
 def sxor(a: str, b: str) -> str:
     return ''.join(chr(ord(a) ^ ord(b)) for a, b in zip(a, b))
+
+
+def english_score(msg: str) -> float:
+    msg = msg.lower()
+    character_frequencies = {
+        'a': .08167, 'b': .01492, 'c': .02782, 'd': .04253, 'e': .12702, 'f': .02228, 'g': .02015, 'h': .06094, 'i': .06094, 'j': .00153, 'k': .00772, 'l': .04025, 'm': .02406, 'n': .06749, 'o': .07507, 'p': .01929, 'q': .00095, 'r': .05987, 's': .06327, 't': .09056, 'u': .02758, 'v': .00978, 'w': .02360, 'x': .00150, 'y': .01974, 'z': .00074, ' ': .13000
+    }
+
+    score = sum([character_frequencies.get(char, 0) for char in msg])
+    for char in msg:
+        if not char.isalpha():
+            score -= 0.1
+        if char == '\n':
+            score -= 0.2
+    return score
+
+
+def single_xor_cipher(msg: bytes, key: int) -> tuple[bytes, float]:
+    # Inverse of XOR is XOR
+    xor = bytes(a ^ key for a in msg)
+    return xor, english_score(str(xor, "utf-8"))
--- set_one.py
+++ set_one.py
@@ -5,6 +5,8 @@ Set 1 of Cryptopals
 https://cryptopals.com/sets/1
 """
 
+LETTERS = range(0, 128)
+
 
 def challenge_1() -> None:
     """Convert hex to base64
@@ -26,8 +28,28 @@ def challenge_2() -> None:
     print(f"{ch2=}")
 
 
+def challenge_3() -> None:
+    """Single-byte XOR cipher
+    https://cryptopals.com/sets/1/challenges/3"""
+
+    scores = {}
+    msgs = {}
+    msg_bytes = bytes.fromhex(
+        "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+    )
+
+    for letter in LETTERS:
+        res, score = crypt.single_xor_cipher(msg_bytes, letter)
+        msgs[letter] = res
+        scores[letter] = score
+    ch3 = max(scores, key=scores.get)
+    print(f"{ch3=}: {round(scores[ch3], 2)}, {msgs[ch3]}")
+
+
 if __name__ == "__main__":
     print("CHALLENGE 1")
     challenge_1()
     print("\nCHALLENGE 2")
     challenge_2()
+    print("\nCHALLENGE 3")
+    challenge_3()
